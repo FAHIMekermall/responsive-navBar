6d51bfc7562de7ffdbc1ca89fa25121d438a03b3
Samiul Basir Fahim <93071892+FAHIMekermall@users.noreply.github.com>
2022-04-16 04:10:50 +0600

    done

--- src/NavBar/Navbar.js
+++ src/NavBar/Navbar.js
@@ -1,13 +1,33 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link, NavLink } from "react-router-dom"
+import { HiMenu, HiOutlineMenuAlt1 } from "react-icons/hi"
 
 const Navbar = () => {
+	const [menuOpen, setMenuOpen] = useState(false)
 	return (
 		<header className="flex justify-between mx-4 md:mx-12 lg:mx-32">
 			<nav>
-				<Link className="text-6xl font-sans font-semibold" to="/">Logo</Link>
+				<Link className="text-6xl font-sans font-semibold" to="/">
+					Logo
+				</Link>
 			</nav>
-			<nav className="hidden md:block">
+			<div className="z-50">
+				{menuOpen ? (
+					<HiOutlineMenuAlt1
+						size={"2em"}
+						onClick={() => setMenuOpen(false)}
+					/>
+				) : (
+					<HiMenu
+						size={"2em"}
+						onClick={() => setMenuOpen(true)}
+					></HiMenu>
+				)}
+			</div>
+			<nav
+				style={!menuOpen ? { right: "-220px" } : { right: "20px" }}
+				className="flex flex-col md:flex-row bg-white absolute top-8 "
+			>
 				<NavLink className="font-bold py-4 px-6" to="/">
 					Home
 				</NavLink>
